1e0f0485f1cdf8625d69498e631f8c86d0b91097
Leyla Mamedova <leka2101@mail.ru>
2017-11-16 04:22:57 +0300

    change imgs to 2x

--- src/Footer/Styled.js
+++ src/Footer/Styled.js
@@ -4,6 +4,7 @@ import arrowdown from "./arrow-down.png";
 export const Footer = styled.footer`
   width: 100%;
   padding-top: 48px;
+  margin-top: 64px;
   box-shadow: 0px 0px 1px rgba(72, 72, 72, 0.3);
   line-height: 24px;
 `;
