8b0e2868e300e03d640627287fd267fa4098be8a
Leyla Mamedova <leka2101@mail.ru>
2017-11-18 08:47:37 +0300

    add styles to mobile popup, fix dates.js

--- src/Homes/Dates.js
+++ src/Homes/Dates.js
@@ -5,6 +5,7 @@ import "react-dates/lib/css/_datepicker.css";
 import { DayPickerRangeController } from "react-dates";
 import styled from "styled-components";
 import moment from "moment";
+import close from "./close.svg";
 
 const Desktop = styled.div`
   display: none;
@@ -22,11 +23,43 @@ const Tablet = styled.div`
   }
 `;
 const Mobile = styled.div`
+  width: 100vw;
   display: block;
   @media (min-width: 768px) {
     display: none;
   }
 `;
+const Close = styled.a`
+  width: 16px;
+  height: 16px;
+  background-image: url(${close});
+  background-repeat: no-repeat;
+  background-size: cover;
+  cursor: pointer;
+`;
+
+const Title = styled.div`
+  display: inline-block;
+  font-size: 14px;
+  font-family: "Circular Air Book", "Arial", sans-serif;
+  color: #383838;
+`;
+
+const Reset = styled.button`
+  font-size: 14px;
+  font-family: "Circular Air Book", "Arial", sans-serif;
+  color: #0f7276;
+  border: none;
+  background-color: inherit;
+`;
+
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 16px 8px 40px 8px;
+`;
+
 export default class extends React.Component {
   state = {
     startDate: null,
@@ -34,13 +67,14 @@ export default class extends React.Component {
     focusedInput: "startDate"
   };
   onFocusChange = focusedInput => {
-    console.log(focusedInput);
     this.setState({
       focusedInput: !focusedInput ? "startDate" : focusedInput
     });
   };
   onDatesChange = ({ startDate, endDate }) => {
     this.setState({ startDate, endDate });
+    console.log(this);
+    this.props.dateChanged(startDate, endDate);
   };
 
   render() {
@@ -69,6 +103,11 @@ export default class extends React.Component {
           />
         </Tablet>
         <Mobile>
+          <Header>
+            <Close />
+            <Title>When</Title>
+            <Reset>Reset</Reset>
+          </Header>
           <DayPickerRangeController
             startDate={this.state.startDate}
             endDate={this.state.endDate}
