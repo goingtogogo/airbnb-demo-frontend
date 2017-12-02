f50788dfe24292fe2b70488c577726887df735c8
Leyla Mamedova <leka2101@mail.ru>
2017-12-02 22:39:36 +0300

    add footer to langing

--- src/Landing/index.js
+++ src/Landing/index.js
@@ -5,15 +5,19 @@ import Experiences from "./Experiences/";
 import Homes from "./Homes/";
 import Popular from "./Popular/";
 import Featured from "./Featured/";
+import Footer from "../Footer";
 
 export default function() {
   return (
-    <Wrapper>
-      <Explore />
-      <Experiences />
-      <Homes />
-      <Popular />
-      <Featured />
-    </Wrapper>
+    <div>
+      <Wrapper>
+        <Explore />
+        <Experiences />
+        <Homes />
+        <Popular />
+        <Featured />
+      </Wrapper>
+      <Footer />
+    </div>
   );
 }
